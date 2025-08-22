7ede2488878ab301248b64ed2c8e27971695b749
professoreyebeam-coder <professoreyebeam@gmail.com>
2025-08-22 12:32:20 +1000

    Update debtdivers.js

--- js/debtdivers.js
+++ js/debtdivers.js
@@ -233,11 +233,7 @@ const submitMissionReport = async (isMissionSucceeded) => {
     let deathsDifficultyModifier = 10;
     let accidentalsDifficultyModifier = 20;
     const starsEarnedModifier = 
-      if parseInt(missionDifficultyInput.value, 10) =< 7
-      parseInt(starsEarnedInput.value, 10) *
-      parseInt(missionDifficultyInput.value, 10) *
-      2;
-      else if parseInt(missionDifficultyInput.value, 10) = 8 || parseInt(missionDifficultyInput.value, 10) = 9
+      if parseInt(missionDifficultyInput.value, 10) = 8 || parseInt(missionDifficultyInput.value, 10) = 9
       parseInt(starsEarnedInput.value, 10) *
       (3+parseInt(missionDifficultyInput.value, 10) *
       2);
@@ -245,6 +241,10 @@ const submitMissionReport = async (isMissionSucceeded) => {
         parseInt(starsEarnedInput.value, 10) *
       (5+parseInt(missionDifficultyInput.value, 10) *
       2);
+      else
+        parseInt(starsEarnedInput.value, 10) *
+      parseInt(missionDifficultyInput.value, 10) *
+      2;
 
     const superSamplesModifier = superSamplesCollectedInput.value * 12;
 
